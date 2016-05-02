ac668765f9f14881b258ef9663cde87a7face352
adamlingo <adamlingo@yahoo.com>
2016-05-02 18:42:59 -0500

    Minitest reporters code to test in Green/Red

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,19 +1,27 @@
 require "test_helper"
 
 class PagesControllerTest < ActionController::TestCase
+  # "setup" is run before each test automatically
+  def setup
+    @base_title = "Ruby Blog Tutorial"
+  end
+
   def test_home
     get :home
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   def test_help
     get :help
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   def test_about
     get :about
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
-  
+
 end
