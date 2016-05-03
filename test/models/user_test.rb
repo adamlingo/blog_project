7d2240bb5b0ef2eb1d532695709555ff8defe553
adamlingo <adamlingo@yahoo.com>
2016-05-03 16:13:18 -0500

    erd and models test for user (custom)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,12 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  def user
-    @user ||= User.new
+  # setup run before each test, run to validate user
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   def test_valid
-    assert user.valid?
+    assert @user.valid?
   end
 end
