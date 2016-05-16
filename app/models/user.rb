0c866b92734d7e5065394d92e72b02747d2758ac
adamlingo <adamlingo@yahoo.com>
2016-05-16 18:46:22 -0500

    constant for regular expression email format (REGEX)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   # validates method, same as validates(:name, presence: true)
   validates :name, presence: true
-  validates :email, presence: true
+  # valid e-mail REGEX, or regular expression defined as a CONSTANT
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
 end
