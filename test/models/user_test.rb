3caf91a375838493c9fbc2527d7007257c38be43
adamlingo <adamlingo@yahoo.com>
2016-05-16 18:41:36 -0500

    test email format validity and presence

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,8 +10,24 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+  # validity of name and email contingent on not being blank
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      # inspect would give literal address that causes failure
+      assert_not @user.valid?, "#{valid_address.inspect} should be valid"
+    end
   end
 end
