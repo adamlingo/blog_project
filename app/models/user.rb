89378c81d6237e76330086cdf065ace5b653af9a
adamlingo <adamlingo@yahoo.com>
2016-05-16 20:00:44 -0500

    Validations for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # callback before record is saved, indicating lowercase conversion
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   # validates method, same as validates(:name, presence: true)
   validates :name, presence: true
   # valid e-mail REGEX, or regular expression defined as a CONSTANT
@@ -8,4 +8,6 @@ class User < ActiveRecord::Base
   # uniqueness is inferred as true, but you can set case sensitivity
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
               uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
