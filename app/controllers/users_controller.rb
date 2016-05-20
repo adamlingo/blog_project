af6562d918d5cc6bb54d577f02ec851fe72b9d85
adamlingo <adamlingo@yahoo.com>
2016-05-20 16:35:18 -0500

    Comment-out debugger

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    # call 'byebug' in rails server that acts as rails console
+    # debugger
   end
 
   def new
