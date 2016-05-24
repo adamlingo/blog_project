7f22fe090845597a99d1d68055ec3efd2d531da3
adamlingo <adamlingo@yahoo.com>
2016-05-24 17:01:34 -0500

    test for valid login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   
+  # :adam is found in fixtures/users.yml
+  def setup
+    @user = users(:adam)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -12,4 +17,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
   
+  # Run rake test with specific test in a test file like this:
+  # TEST=test/integration/users_login_test.rb TESTOPTS="--name test_login_with_valid_information"
+  test "login with valid information" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    # visit targeted page
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
 end
