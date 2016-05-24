04f8c72351459c3919dc05d356f88f9f0a5dfad0
adamlingo <adamlingo@yahoo.com>
2016-05-24 16:46:55 -0500

    login and redirect to user's page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       # .now makes flash contents disappear upon additional request
       flash.now[:danger] = 'Invalid email/password combination'
