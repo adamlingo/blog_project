5e11a31c3467c2809176b3d95deca58a64fe84fe
adamlingo <adamlingo@yahoo.com>
2016-05-26 08:22:39 -0500

    SessionsHelper module has methods for logout plus other doc.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,23 @@
 module SessionsHelper
 
+  #login user
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  #return logged in user
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  #return true if user is logged in
   def logged_in?
     !current_user.nil?
+  end
+
+  #logout user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
