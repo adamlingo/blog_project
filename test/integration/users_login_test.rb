c052a87e6fda79488d07227dcff390ed8ec11dcf
adamlingo <adamlingo@yahoo.com>
2016-05-26 08:24:03 -0500

    Update tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,15 +19,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   
   # Run rake test with specific test in a test file like this:
   # TEST=test/integration/users_login_test.rb TESTOPTS="--name test_login_with_valid_information"
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
+    assert is_logged_in?
     assert_redirected_to @user
-    # visit targeted page
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
