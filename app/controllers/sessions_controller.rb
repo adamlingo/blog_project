50369dd0e30929d37f1d9df6fa8e82c97e11a173
adamlingo <adamlingo@yahoo.com>
2016-06-01 15:00:11 -0500

    change redirect to helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in user
       # ternary operator for if/then
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       # .now makes flash contents disappear upon additional request
       flash.now[:danger] = 'Invalid email/password combination'
