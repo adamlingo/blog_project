344a4fb1a7839e19cef91e43c0f9f0a6f5ae32c8
adamlingo <adamlingo@yahoo.com>
2016-06-07 15:45:25 -0500

    Syntax fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # check before edit and update that user is logged in and current user
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: [:destroy]
+  before_action :admin_user,     only: :destroy
 
   def index
     # @users = User.all
@@ -74,6 +74,7 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless @user == current_user
     end
 
+    # Confirm an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
