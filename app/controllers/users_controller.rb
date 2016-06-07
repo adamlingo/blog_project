ae6a56333e4cb84a4b59086c5bf2ba34354565f0
adamlingo <adamlingo@yahoo.com>
2016-06-07 15:20:03 -0500

    Destroy method and before action clarity on destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   # check before edit and update that user is logged in and current user
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:destroy]
 
   def index
     # @users = User.all
@@ -44,6 +45,12 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   private
