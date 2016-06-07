29605f5803c936c6b781247e7b27ca43a2fe5863
adamlingo <adamlingo@yahoo.com>
2016-06-07 13:41:01 -0500

    index test for Users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
-  def test_sanity
-    flunk "Need real tests"
+  
+  def setup
+    @user = users(:adam)
+  end
+
+  test "index including pagination" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    # check for div requiring pagination class
+    assert_select 'div.pagination'
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
   end
 end
